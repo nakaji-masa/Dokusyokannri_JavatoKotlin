e26908b43ba1e3f7e84c21546b2af1d7eec8bc47
nakaji-masa <maketotheago@gmail.com>
2020-10-14 21:52:51 +0900

    登録時の処理、レイアウトの追加

---
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/RegisterActivity.kt
@@ -0,0 +1,145 @@
+package android.wings.websarva.dokusyokannrijavatokotlin
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
+import java.io.ByteArrayOutputStream
+
+class RegisterActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val id = intent.getIntExtra("_id", -1)
+
+        if(id != -1) {
+            val helper = DataBaseHelper(this)
+
+            val db = helper.writableDatabase
+
+            try {
+                val sql =
+                    "SELECT bookname, deadline, bookNotice, bookActionplan, bookImage FROM BookList WHERE _id ="  + id
+
+                val cursor = db.rawQuery(sql, null)
+
+                var arrayByte = ByteArray(0)
+
+                while (cursor.moveToNext()) {
+                    book_name_input.setText(cursor.getString(cursor.getColumnIndex("bookName")))
+                    book_deadline_input.setText(cursor.getString(cursor.getColumnIndex("deadline")))
+                    book_notice_input.setText(cursor.getString(cursor.getColumnIndex("bookNotice")))
+                    book_actionPlan_input.setText(cursor.getString(cursor.getColumnIndex("bookActionplan")))
+                    arrayByte = cursor.getBlob(cursor.getColumnIndex("bookImage"))
+                    book_image.setImageBitmap(
+                        BitmapFactory.decodeByteArray(
+                            arrayByte,
+                            0,
+                            arrayByte.size
+                        )
+                    )
+                }
+
+            } finally {
+                db.close()
+            }
+        }
+
+        save_button.setOnClickListener{
+            if (id == -1) {
+                if (book_name_input.text.toString() == null || book_deadline_input.text.toString() == null ||
+                    book_notice_input.text.toString() == null || book_actionPlan_input.text.toString() == null
+                ) {
+                    Toast.makeText(this, "未入力の項目があります。", Toast.LENGTH_LONG).show()
+                } else {
+                    //DataBaseHelperクラスのインスタンスを作成
+                    val helper = DataBaseHelper(this)
+
+                    //データーベースに書き込みを加える
+                    val db = helper.writableDatabase
+
+                    try {
+                        val sqlInsert =
+                            "INSERT INTO BookList (bookname, deadline, bookNotice, bookActionplan, bookImage, bookidCount) VALUES(?, ?, ?, ?, ?, ?)"
+
+                        //レコードの追加
+                        val stmt = db.compileStatement(sqlInsert)
+
+                        //保存したい値を格納
+                        stmt.bindString(1, book_name_input.text.toString())
+                        stmt.bindString(2, book_deadline_input.text.toString())
+                        stmt.bindString(3, book_notice_input.text.toString())
+                        stmt.bindString(4, book_actionPlan_input.text.toString())
+
+                        //bitmap型からbyteArray型に変換
+                        val byteArrayOutputStream = ByteArrayOutputStream()
+                        var bitmap = (book_image.drawable as BitmapDrawable).bitmap
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+
+                        //保存する値を格納
+                        stmt.bindBlob(5, byteArrayOutputStream.toByteArray())
+
+                        //sql文の実行
+                        stmt.executeInsert()
+
+                    } finally {
+                        db.close()
+                    }
+                    val intent = Intent(this, MainActivity::class.java)
+
+                    startActivity(intent)
+
+                }
+
+            } else {
+                val helper = DataBaseHelper(this)
+
+                val db = helper.writableDatabase
+
+                try {
+                    val sql =
+                        "UPDATE BookList SET bookname = ?, deadline = ?, bookNotice = ?, bookActionplan = ?, bookImage = ?  WHERE _id = ?"
+
+                    val stmt = db.compileStatement(sql)
+
+                    stmt.bindString(1, book_name_input.text.toString())
+                    println(book_name_input.text.toString())
+                    stmt.bindString(2, book_deadline_input.text.toString())
+                    stmt.bindString(3, book_notice_input.text.toString())
+                    stmt.bindString(4, book_actionPlan_input.text.toString())
+
+                    val byteArrayOutputStream = ByteArrayOutputStream()
+                    val bitmap = (book_image.drawable as BitmapDrawable).bitmap
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+                    stmt.bindBlob(5, byteArrayOutputStream.toByteArray())
+                    stmt.bindLong(6, id.toLong())
+
+                    stmt.executeUpdateDelete()
+                    
+
+                }finally {
+                    db.close()
+                }
+
+                val intent = Intent(this, MainActivity::class.java)
+
+                startActivity(intent)
+            }
+
+
+        }
+
+        back_button.setOnClickListener {
+            finish()
+        }
+    }
+}
