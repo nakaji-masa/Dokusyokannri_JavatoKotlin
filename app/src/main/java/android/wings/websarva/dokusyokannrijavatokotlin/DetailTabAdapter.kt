55a0b2d995014b46e79c84d7598bcd9de39bc632
nakaji-masa <maketotheago@gmail.com>
2020-10-16 20:43:32 +0900

    タブの処理を記述

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/DetailTabAdapter.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/DetailTabAdapter.kt
@@ -1,4 +1,40 @@
 package android.wings.websarva.dokusyokannrijavatokotlin
 
-class DetailTabAdapter {
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+@Suppress("DEPRECATION")
+class DetailTabAdapter(fm : FragmentManager, val context : Context) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        //タブの数を返す
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+       //押しているタブによって返すフラグメントを変える
+        when(position) {
+            0 -> {
+                return DetailFragment()
+            }
+
+            else -> {
+                return DetailFragment2()
+            }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        //タブの文字を決める
+        when(position) {
+            0 -> {
+                return "本の詳細"
+            }
+
+            else -> {
+                return "アクションプランの経過"
+            }
+        }
+    }
 }
