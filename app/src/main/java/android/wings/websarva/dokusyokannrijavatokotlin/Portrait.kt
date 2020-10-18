bd2b9be7a196254cec5633d1bf9ad246436ac043
nakaji-masa <maketotheago@gmail.com>
2020-10-18 13:07:28 +0900

    バーコード　縦画面にする

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/Portrait.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/Portrait.kt
@@ -2,11 +2,9 @@ package android.wings.websarva.dokusyokannrijavatokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.journeyapps.barcodescanner.CaptureActivity
 
-class Portrait : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_portrait)
-    }
+class Portrait : CaptureActivity() {
 }
+
+
