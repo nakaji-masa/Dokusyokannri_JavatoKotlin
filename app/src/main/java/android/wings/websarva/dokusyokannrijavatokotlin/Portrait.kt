c294a79b628f243b6bccb921a83f4f9e57fa4434
nakaji-masa <maketotheago@gmail.com>
2020-10-18 13:05:27 +0900

    バーコードを縦画面にするアクティビティーの追加

---
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/Portrait.kt
@@ -0,0 +1,12 @@
+package android.wings.websarva.dokusyokannrijavatokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Portrait : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_portrait)
+    }
+}
