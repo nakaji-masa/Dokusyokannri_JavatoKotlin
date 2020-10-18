26abb6a8856234fb4543a2dde64a400548eeb311
nakaji-masa <maketotheago@gmail.com>
2020-10-18 13:07:54 +0900

    パーミッションチェックの追加

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/RegisterActivity.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package android.wings.websarva.dokusyokannrijavatokotlin
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -7,17 +8,24 @@ import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_register.*
 import java.io.ByteArrayOutputStream
 
 class RegisterActivity : AppCompatActivity() {
 
+    companion object {
+        const val CAMERA_REQUEST_CODE = 1
+        const val CAMERA_PERMISSION_REQUEST_CODE = 2
+        const val BARCODE_PERMISSION_REQUEST_CODE = 3
+    }
 
-    val REQUEST_IMAGE_CAPTURE = 1
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -150,15 +158,36 @@ class RegisterActivity : AppCompatActivity() {
 
 
         book_image.setOnClickListener {
-            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {takePictureIntent ->
-                takePictureIntent.resolveActivity(packageManager)?.also {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            //ユーザーの端末にカメラ機能があるかどうかの確認
+            //Intentに入っている機能を使いたい。(MediaStore.ACTION_IMAGE_CAPTURE)
+            //resolveActivityで確認する
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).resolveActivity(packageManager)?.let {
+                //nullが返されなければ、カメラを起動かパーミッションチェックを行う。
+                if (checkCameraPermission()) {
+                    takePicture()
+                } else {
+                    grantCameraPermission()
                 }
-            }
+            } ?: Toast.makeText(this, "カメラを扱うアプリがありません", Toast.LENGTH_LONG).show()
+                 //カメラ機能がなければトーストを表示する。
         }
+
 
         back_button.setOnClickListener {
             finish()
+        }
+
+        test_button.setOnClickListener {
+           Intent(MediaStore.ACTION_IMAGE_CAPTURE).resolveActivity(packageManager)?.let {
+               if (checkCameraPermission()) {
+                   takeBarCode()
+
+               } else {
+                   grantBarCodePermission()
+               }
+
+           } ?: Toast.makeText(this, "カメラを扱うアプリがありません", Toast.LENGTH_LONG).show()
+
         }
 
 
@@ -166,9 +195,84 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+
+
+        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
             val imageBitmap = data?.extras?.get("data") as Bitmap
             book_image.setImageBitmap(imageBitmap)
         }
+
+        else {
+            val resultBarcode = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+       }
+
+
+
+
     }
+
+    //バーコードリーダー起動メソッド
+    private fun takeBarCode() {
+        val intentIntegrator = IntentIntegrator(this)
+            .setBeepEnabled(false)
+            .apply {
+                captureActivity = Portrait::class.java
+            }.initiateScan()
+    }
+
+    //カメラアプリの起動メソッド
+    private fun takePicture() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+
+    //カメラ機能のパーミッションを持っているかどうかの確認
+    //持っていればtrueを返す。
+    private fun checkCameraPermission() = PackageManager.PERMISSION_GRANTED ==
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+            // Manifest.permission.CAMERAっていう許可とっているかな？
+
+    //パーミッションを得るための関数
+    private fun grantCameraPermission() =
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE)
+
+    //バーコードからパーミッションを得るためのメソッド
+    private fun  grantBarCodePermission() =
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.CAMERA),
+            BARCODE_PERMISSION_REQUEST_CODE)
+
+
+    //パーミッションを得たときにカメラの起動する関数。
+    //取得の確認をするメソッド。
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                takePicture()
+            }
+        }
+
+        //バーコードリクエストコードを持っていたらバーコードを起動する。
+        if(requestCode == BARCODE_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takeBarCode()
+            }
+        }
+    }
+
+
+
+
+
 }
