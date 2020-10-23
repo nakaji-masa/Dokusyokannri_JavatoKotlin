9a2561bf82f162579db24c26da9f76c107f5154f
DESKTOP-GRG1A57\user <maketotheago@gmail.com>
2020-10-23 22:37:34 +0900

    Fragment realmに変更

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/DetailFragment.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/DetailFragment.kt
@@ -7,26 +7,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.realm.Realm
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.view.*
+import kotlinx.android.synthetic.main.fragment_detail.*
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var id: Int?  = null
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            id = it.getInt("_id")
+            id = it.getInt("id")
+            println(id!!)
         }
     }
 
@@ -38,43 +38,26 @@ class DetailFragment : Fragment() {
         //返すビュー変数を作成
         val view = inflater.inflate(R.layout.fragment_detail, container, false)
 
-        val helper = DataBaseHelper(activity)
+        realm = Realm.getDefaultInstance()
 
-        val db: SQLiteDatabase = helper.getWritableDatabase()
+        println(id)
+        val book = realm.where<BookListObject>().equalTo("id", id).findFirst()
 
-        try {
-            val sql =
-                "SELECT bookName, deadline, bookNotice, bookActionPlan, bookImage FROM BookList WHERE _id =$id"
-            val cursor = db.rawQuery(sql, null)
-
-            var arrayByte = ByteArray(0)
-
-            while (cursor.moveToNext()) {
-                view.book_name_view.text = cursor.getString(cursor.getColumnIndex("bookName"))
-                view.book_deadline_view.text = cursor.getString(cursor.getColumnIndex("deadline"))
-                view.book_notice_view.text = cursor.getString(cursor.getColumnIndex("bookNotice"))
-                view.book_actionplan_view.text = cursor.getString(cursor.getColumnIndex("bookActionPlan"))
-                arrayByte = cursor.getBlob(cursor.getColumnIndex("bookImage"))
-                view.book_image_view.setImageBitmap(BitmapFactory.decodeByteArray(arrayByte, 0, arrayByte.size))
-            }
-
-        } finally {
-            db.close()
-        }
-
+        view.book_title_view.text = book?.title
+        view.book_date_view.text = book?.date
+        view.book_notice_view.text = book?.notice
+        view.book_actionPlan_view.text = book?.actionPlan
+        view.book_image_view.setImageBitmap(book?.image?.size?.let {
+            BitmapFactory.decodeByteArray(
+                book?.image, 0,
+                it
+            )
+        })
         return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1 : Int) : DetailFragment{
             //インスタンスの生成
@@ -84,7 +67,7 @@ class DetailFragment : Fragment() {
             val bundle = Bundle()
 
             //値を格納
-            bundle.putInt("_id", param1)
+            bundle.putInt("id", param1)
 
             //フラグメントにセット
             fragment.arguments = bundle
