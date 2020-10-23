53d0baebcc9de485bd6db30abe8dc4f70eef2ce3
DESKTOP-GRG1A57\user <maketotheago@gmail.com>
2020-10-23 22:37:00 +0900

    MyApplicationの追加

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/MyApplication.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/MyApplication.kt
@@ -1,4 +1,23 @@
 package android.wings.websarva.dokusyokannrijavatokotlin
 
-class MyApplication {
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    //Realmの初期化
+        Realm.init(this)
+
+        //Realmデータベースの設定
+        val config = RealmConfiguration.Builder()
+            .name("bookList")
+            .build()
+
+        //設定をセット
+        Realm.setDefaultConfiguration(config)
+
+    }
 }
