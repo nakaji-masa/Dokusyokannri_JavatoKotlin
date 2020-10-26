ab69ed5318786cfd235c45fdfde8796e5e447324
DESKTOP-GRG1A57\user <maketotheago@gmail.com>
2020-10-26 21:35:31 +0900

    recyclerviewを削除　containerを作成

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/MainActivity.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/MainActivity.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import android.widget.AdapterView.OnItemClickListener
+import android.wings.websarva.dokusyokannrijavatokotlin.R.id.book_read_item
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,43 +21,50 @@ import java.nio.file.Files.find
 class MainActivity : AppCompatActivity() {
 
     private lateinit var realm : Realm
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //アクションバーのセット
         setSupportActionBar(parent_toolbar)
 
+        val fragment = BookListFragment()
 
-        realm = Realm.getDefaultInstance()
+        val transaction = supportFragmentManager.beginTransaction()
 
-        val bookList = realm.where(BookListObject::class.java).findAll()
+        transaction.replace(R.id.container, fragment)
 
-        val adapter = BookListAdapterMain(this, bookList, true)
+        transaction.commit()
 
-        //レイアウトマネージャーなどの設定
-        book_recyclerview.setHasFixedSize(true)
-        book_recyclerview.layoutManager = LinearLayoutManager(this)
-        book_recyclerview.adapter = adapter
+        bottom_navigationView.setOnNavigationItemSelectedListener {item ->
 
-        //リスナーの設定
-        adapter.setOnItemClickListener(object :  BookListAdapterMain.OnItemClickListener {
-            override fun onItemClickListener(view: View, position: Int, clickedId: Int?) {
-                val intent = Intent(view.context, Detail::class.java)
-                intent.putExtra("id", clickedId)
-                startActivity(intent)
+            when(item.itemId){
+                R.id.book_read_item -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, BookListFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.graph_item -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, GraphFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.settings_item -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, SettingsFragment())
+                        .commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+
             }
-        })
+            return@setOnNavigationItemSelectedListener false
+        }
 
-
-        add_fab.setOnClickListener(View.OnClickListener {
-
-            val intent = Intent(this, RegisterActivity::class.java)
-
-            startActivity(intent)
-
-        })
 
     }
 }
