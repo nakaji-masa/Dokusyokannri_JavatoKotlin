fb0819ef021b33c9f8dfbefca7bacbe97fd38642
DESKTOP-GRG1A57\user <maketotheago@gmail.com>
2020-11-03 19:15:48 +0900

    コードを見やすく

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/Portrait.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/Portrait.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.journeyapps.barcodescanner.CaptureActivity
 
-class Portrait : CaptureActivity() {
-}
+class Portrait : CaptureActivity()
 
 
