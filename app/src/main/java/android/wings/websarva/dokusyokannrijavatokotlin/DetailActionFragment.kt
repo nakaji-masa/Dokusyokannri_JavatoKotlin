1dddc9775a37cbda1479ca1481e41e1610cae540
DESKTOP-GRG1A57\user <maketotheago@gmail.com>
2020-11-07 17:11:56 +0900

    アクションプランの経過を表示するフラグメントを作成

---
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/DetailActionFragment.kt
@@ -0,0 +1,75 @@
+package android.wings.websarva.dokusyokannrijavatokotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.Realm
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.fragment_action_detail.*
+
+
+class DetailActionFragment : Fragment() {
+    private var id: Int? = null
+    private lateinit var realm: Realm
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            id = it.getInt("id")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_action_detail, container, false)
+        realm = Realm.getDefaultInstance()
+        val book = realm.where<BookListObject>().equalTo("id", id).findAll()
+
+        //アダプターをセットする。
+        val adapter = DetailActionAdapter(view.context, book, true)
+
+        //リサイクラービューの設定
+        val recyclerView = view.findViewById<RecyclerView>(R.id.action_recycler_view)
+        recyclerView.adapter = adapter
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        add_actionPlan.setOnClickListener {
+            val intent = Intent(view.context, ActionRegisterActivity::class.java)
+            intent.putExtra("id", id)
+            startActivity(intent)
+        }
+
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(id: Int) =
+            DetailActionFragment().apply {
+                arguments = Bundle().apply {
+                    val fragment = DetailActionFragment()
+
+                    val bundle = Bundle()
+
+                    bundle.putInt("id", id)
+
+                    fragment.arguments = bundle
+
+                    return fragment
+                }
+            }
+    }
+}
