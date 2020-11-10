f59fcc1a20d4a9fb1b2ee85b05e8104d330affc1
DESKTOP-GRG1A57\user <maketotheago@gmail.com>
2020-11-10 23:04:42 +0900

    名前の変更

--- app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/PortraitActivity.kt
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/PortraitActivity.kt
@@ -1,9 +1,7 @@
 package android.wings.websarva.dokusyokannrijavatokotlin
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.journeyapps.barcodescanner.CaptureActivity
 
-class Portrait : CaptureActivity()
+class PortraitActivity : CaptureActivity()
 
 
