3822c1b361cb337e0ef3652866bab177d33375c2
DESKTOP-GRG1A57\user <maketotheago@gmail.com>
2020-11-10 23:03:46 +0900

    日付を取得するオブジェクト

---
+++ app/src/main/java/android/wings/websarva/dokusyokannrijavatokotlin/GetDateObject.kt
@@ -0,0 +1,13 @@
+package android.wings.websarva.dokusyokannrijavatokotlin
+
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Date
+
+object GetDateObject {
+    fun getToday(): String {
+        val sdf = SimpleDateFormat("yyyy年MM月dd日", Locale.JAPAN)
+        val date = Date()
+        return sdf.format(date)
+    }
+}
